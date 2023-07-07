795bb9ee43f03efbfbab7551e3951bd68cbad116
hobosock <76829496+hobosock@users.noreply.github.com>
2023-07-06 21:40:03 -0400

    converts files now using shell command
    - need to get clean filename (strip .flac)
    - needs to check if file exists before converting
    - then check if conversion was successful
    - then delete if conversion was successful

--- flac_converter.py
+++ flac_converter.py
@@ -2,9 +2,26 @@ import os
 
 # ffmpeg -i "01 Once More, Jubilations Shall Ring Through The Halls Of Durin's Folk.flac" -ab 320k -map_metadata 0 -id3v2_version 3 output.mp3
 
-folder = "/home/seth/Music/Gimli- Son of Glóin (copy 1)/"
-dir_list = os.listdir(folder)
 
-for i in dir_list:
-    if os.path.isdir(folder + i):
-        print(i)
+def run_fast_scandir(dir, ext):    # dir: str, ext: list
+    subfolders, files = [], []
+
+    for f in os.scandir(dir):
+        if f.is_dir():
+            subfolders.append(f.path)
+        if f.is_file():
+            if os.path.splitext(f.name)[1].lower() in ext:
+                files.append(f.path)
+
+    for dir in list(subfolders):
+        sf, f = run_fast_scandir(dir, ext)
+        subfolders.extend(sf)
+        files.extend(f)
+    return subfolders, files
+
+
+folder = "/home/seth/Music/Gimli- Son of Glóin (copy 1)/"
+subfolders, files = run_fast_scandir(folder, [".flac"])
+
+for i in files:
+    os.system("ffmpeg -i " + "\"" + i + "\"" + " -ab 320k -map_metadata 0 -id3v2_version 3 " + "\"" + i + "\"" + ".mp3")
