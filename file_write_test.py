1cc7fd67734c11becec7d8c3d2e10dc6b4f9d9cb
hobosock <76829496+hobosock@users.noreply.github.com>
2023-07-10 12:03:36 -0400

    added reports

---
+++ file_write_test.py
@@ -0,0 +1,5 @@
+test_string = "test string"
+
+testid = open("test.txt", "a")
+testid.write(test_string)
+testid.close()
--- flac_converter.py
+++ flac_converter.py
@@ -18,9 +18,14 @@ def run_fast_scandir(dir, ext):    # dir: str, ext: list
     return subfolders, files
 
 
-folder = "/home/seth/Music/Gimli- Son of Glóin (copy 1)/"
+folder = "/media/seth/3037-6134/Music/"
 subfolders, files = run_fast_scandir(folder, [".flac"])
 mp3_subfolders, mp3_files = run_fast_scandir(folder, ".mp3")
+
+# make some reports to write to file
+existed = ""
+failed = ""
+passed = ""
 
 for i in files:
     # remove .flac from filename
@@ -28,8 +33,24 @@ for i in files:
     print(filename)
     if os.path.isfile(filename):  # check if mp3 file already exists
         print(filename + " already exists, skipping...")
+        existed = existed + "\n" + filename
     else:  # convert if not
-        os.system("ffmpeg -i " + "\"" + i + "\"" + " -ab 320k -map_metadata 0 -id3v2_version 3 " + "\"" + filename + "\"")
+        os.system("ffmpeg -i " + "\"" + i + "\"" +
+                  " -ab 320k -map_metadata 0 -id3v2_version 3 " + "\"" + filename + "\"")
     # check if mp3 exists now (converted correctly), then delete .flac
-    if os.path.exists(filename):
-        os.remove(i)
+        if os.path.exists(filename):
+            os.remove(i)
+            passed = passed + "\n" + filename
+        else:
+            failed = failed + "\n" + filename
+
+# write reports to files
+existed_id = open(folder + "existed.txt", "a")
+existed_id.write(existed)
+existed_id.close()
+passed_id = open(folder + "passed.txt", "a")
+passed_id.write(passed)
+passed_id.close()
+failed_id = open(folder + "failed.txt", "a")
+failed_id.write(failed)
+failed_id.close()
